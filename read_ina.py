9396a8e214f9c4ed1ea88001b26ff2ed65013e6a
OscarLange <langeoscar96@googlemail.com>
2022-08-25 11:43:05 +0200

    memory

--- read_ina.py
+++ read_ina.py
@@ -29,7 +29,7 @@ while(i < 10000):
     read_ina219()
     i += 1
 
-file_name = "./test_memory5.csv"
+file_name = "./test_memory7.csv"
 with open(file_name, 'a') as f:
     try:
         for val in stored_values:
