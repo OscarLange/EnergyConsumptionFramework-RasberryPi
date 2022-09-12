93e53d9f527c487e1298bccd24c65632ca8f5c72
OscarLange <langeoscar96@googlemail.com>
2022-09-12 14:48:51 +0200

    linked list

--- read_ina.py
+++ read_ina.py
@@ -29,7 +29,7 @@ while(i < 20000):
     read_ina219()
     i += 1
 
-file_name = "./new_tests_2/test_malloc.csv"
+file_name = "./new_tests_2/test_linkedlist.csv"
 
 with open(file_name, 'a') as f:
     try:
